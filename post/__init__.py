c4b71db79ea5f49c7e86ac4e631142090367fe2d
youjinse <jinseyou@gmail.com>
2021-01-30 15:10:11 +0900

    버전업

---
+++ post/__init__.py
@@ -0,0 +1,3 @@
+
+
+cnx = mysql.connector.connect(user="respac@respac-test-01", password={c3IQbseW1riz}, host="respac-test-01.mariadb.database.azure.com", port=3306, database={respac}, ssl_verify_cert=true)
